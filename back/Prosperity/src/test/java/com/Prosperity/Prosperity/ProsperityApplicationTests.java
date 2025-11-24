176c1fe1f07727890f82f0cbea655ac8ed5f7400
MauricioBairos <bairos206@gmail.com>
2025-11-24 20:42:36 -0300

    BackEnd
    BackEnd em Java

---
+++ back/Prosperity/src/test/java/com/Prosperity/Prosperity/ProsperityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Prosperity.Prosperity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsperityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
